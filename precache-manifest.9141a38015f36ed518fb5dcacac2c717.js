e48eecfe3ba322a9527f1ca65e3d8b3902c1341a
Hanson Kang <48969779+mayoyoyo@users.noreply.github.com>
2020-10-06 05:41:49 -0700

    Updates

---
+++ precache-manifest.9141a38015f36ed518fb5dcacac2c717.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ee1d3be5913ce4b3a85e2d2381ca2d",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "6f82a0e7569e23f4ab75",
+    "url": "/pokedex/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "3157c9bf598996d2bd23",
+    "url": "/pokedex/static/css/main.b69d71ac.chunk.css"
+  },
+  {
+    "revision": "6f82a0e7569e23f4ab75",
+    "url": "/pokedex/static/js/2.909567e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex/static/js/2.909567e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3157c9bf598996d2bd23",
+    "url": "/pokedex/static/js/main.c3fc027b.chunk.js"
+  },
+  {
+    "revision": "5d5b7317cf43a0b8925c",
+    "url": "/pokedex/static/js/runtime-main.2d94522c.js"
+  }
+]);
