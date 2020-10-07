3605c4c156f1b819c5699c657e40aa2121925148
Hanson Kang <48969779+mayoyoyo@users.noreply.github.com>
2020-10-06 22:02:33 -0700

    styling for background and positioning

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import backgroundImage from "./b01.png";
 import "./App.css";
 
 const App = () => {
-  const [pokemon, setPokemon] = useState("");
+  const [pokemon, setPokemon] = useState("bulbasaur");
   const [data, setData] = useState([]);
 
   const handleChange = (event) => {
@@ -26,25 +26,34 @@ const App = () => {
     } catch (error) {
       console.log(error);
     }
+
+    console.log(data);
   };
 
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <label>
-          <input
-            className="form-control"
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-            onChange={handleChange}
-            placeholder="enter pokemon name"
-          />
-        </label>
-      </form>
+    <div className="App" style={{ background: `url(${backgroundImage})` }}>
+      <div className="header">
+        <img className="logo" src="https://i.ibb.co/Vq98tN8/logo.png" />
+        <form className="searchbar" onSubmit={handleSubmit}>
+          <label>
+            <input
+              className="form-control"
+              type="text"
+              placeholder="Search"
+              aria-label="Search"
+              onChange={handleChange}
+              placeholder="enter pokemon name"
+            />
+          </label>
+        </form>
+      </div>
+
       {data.map((data) => {
         return (
-          <div>
+          <div
+            className="card"
+            style={{ background: `url(${backgroundImage})` }}
+          >
             <div className="name">
               #{data.id}. {data.species.name.toUpperCase()}
             </div>
@@ -169,6 +178,38 @@ const App = () => {
               <div className="stat">type: {data.types[0].type.name}</div>
               <div className="stat">height: {data.height * 10}cm</div>
               <div className="stat">weight: {data.weight / 10}kg</div>
+            </div>
+
+            <div className="container">
+              <img src={data.sprites["front_default"]} />
+              <div className="divTable">
+                <div className="divTableBody">
+                  <div className="divTableRow">
+                    <div className="divTableCell">Type</div>{" "}
+                    {Math.round(data.height * 3.9)}"
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Height</div>
+                    <div className="divTableCell">
+                      {" "}
+                      {Math.round(data.height * 3.9)}"
+                    </div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Weight</div>
+                    <div className="divTableCell">
+                      {" "}
+                      {Math.round(data.weight / 4.3)} lbs
+                    </div>
+                  </div>
+                  <div className="divTableRow">
+                    <div className="divTableCell">Number of Battles</div>
+                    <div className="divTableCell">
+                      {data.game_indices.length}
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         );
