a4a1ccec548b245fb70553f253800fc40b453d90
Hanson Kang <48969779+mayoyoyo@users.noreply.github.com>
2020-10-07 04:53:09 -0700

    formatting and commenting

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
 import React, { useState } from "react";
-import axios from "axios";
-import "bootstrap/dist/css/bootstrap.min.css";
-import backgroundImage from "./b01.png";
+import axios from "axios"; // import axios for handling HTTP requests
+import "bootstrap/dist/css/bootstrap.min.css"; // import BootStrap
+import backgroundImage from "./b01.png"; // import background image
 import "./App.css";
 
 const App = () => {
-  const [pokemon, setPokemon] = useState("bulbasaur");
-  const [data, setData] = useState([]);
+  const [pokemon, setPokemon] = useState(""); // Pokemon name field
+  const [data, setData] = useState([]); // Pokemon data obtained from PokeAPI
 
+  // function updating the value of query
   const handleChange = (event) => {
+    // API accepts Pokemon name in lowercase, so convert to lowercase
     setPokemon(event.target.value.toLowerCase());
   };
+
+  // function obtaining the current value of query
   const handleSubmit = (event) => {
     event.preventDefault();
     getPokemon();
   };
 
+  // function obtaining Pokemon data from query
+  const getPokemon = async () => {
+    const dataArray = []; // array storing data
+    try {
+      // make HTTP request to PokeAPI
+      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+      const response = await axios.get(url);
+      dataArray.push(response.data);
+      setData(dataArray); // update global data field with new data
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // function for obtaining data for random Pokemon
   const getRandom = async () => {
     const dataArray = [];
     try {
+      // obtain total number of Pokemon in API
       const countUrl = "https://pokeapi.co/api/v2/pokemon-species/?limit=0";
       const countResponse = await axios.get(countUrl);
       const count = countResponse.data.count;
+
+      // generate random number from 1 to count
       const index = Math.floor(Math.random() * count) + 1;
+      // search and obtain relevant Pokemon data using index
       const url = `https://pokeapi.co/api/v2/pokemon/${index}`;
       const response = await axios.get(url);
       dataArray.push(response.data);
@@ -30,24 +53,11 @@ const App = () => {
     } catch (error) {
       console.log(error);
     }
-
-    console.log(data);
   };
 
-  const getPokemon = async () => {
-    const dataArray = [];
-    try {
-      const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-      const response = await axios.get(url);
-      dataArray.push(response.data);
-      setData(dataArray);
-    } catch (error) {
-      console.log(error);
-    }
-
-    console.log(data);
-  };
-
+  // dictionary of Pokemon types mapped to hexcode color
+  // (colors obtained using color picker on image from
+  // https://bulbapedia.bulbagarden.net/wiki/Type)
   const TYPE_COLORS = {
     normal: "#a8a878",
     fighting: "#c03028",
@@ -70,6 +80,7 @@ const App = () => {
   };
 
   return (
+    // set background picture
     <div className="App" style={{ background: `url(${backgroundImage})` }}>
       <div className="header">
         <button type="button" className="btn-primary" onClick={getRandom}>
@@ -109,147 +120,158 @@ const App = () => {
             >
               <div className="row">
                 <div className="col-sm">
-                  <div className="portrait">
+                  <div className="roundedBox">
                     <img id="sprite" src={data.sprites["front_default"]} />
                     <br></br>
-                    {data.types.map((type) => (
-                      <span
-                        key={type.type.name}
-                        className="badge"
-                        style={{
-                          backgroundColor: `${TYPE_COLORS[type.type.name]}`,
-                          color: "white",
-                        }}
-                      >
-                        {type.type.name}
-                      </span>
-                    ))}
+                    {
+                      // map for all Pokemon types
+                      data.types.map((type) => (
+                        <span
+                          key={type.type.name}
+                          className="badge"
+                          style={{
+                            // assign first type color as background color for badge
+                            backgroundColor: `${TYPE_COLORS[type.type.name]}`,
+                            color: "white",
+                          }}
+                        >
+                          {type.type.name}
+                        </span>
+                      ))
+                    }
                   </div>
                 </div>
 
                 <div className="col-sm">
-                  <div className="divTableRow">
-                    <div className="divTableCell">HP</div>
-                    <div className="divTableCell">
-                      {" "}
-                      <div className="progress">
-                        <div
-                          className="progress-bar"
-                          role="progressbar"
-                          style={{
-                            width: `${data.stats[0].base_stat}%`,
-                          }}
-                        >
-                          <small>{data.stats[0].base_stat}</small>
+                  <div className="roundedBox">
+                    <div className="tableRow">
+                      <div className="tableCell">HP</div>
+                      <div className="tableCell">
+                        {" "}
+                        <div className="progress">
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            style={{
+                              width: `${data.stats[0].base_stat}%`,
+                            }}
+                          >
+                            <small>{data.stats[0].base_stat}</small>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="divTableRow">
-                    <div className="divTableCell">ATT</div>
-                    <div className="divTableCell">
-                      {" "}
-                      <div className="progress">
-                        <div
-                          className="progress-bar"
-                          role="progressbar"
-                          style={{
-                            width: `${data.stats[1].base_stat}%`,
-                          }}
-                        >
-                          <small>{data.stats[1].base_stat}</small>
+                    <div className="tableRow">
+                      <div className="tableCell">ATT</div>
+                      <div className="tableCell">
+                        {" "}
+                        <div className="progress">
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            style={{
+                              width: `${data.stats[1].base_stat}%`,
+                            }}
+                          >
+                            <small>{data.stats[1].base_stat}</small>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="divTableRow">
-                    <div className="divTableCell">DEF</div>
-                    <div className="divTableCell">
-                      {" "}
-                      <div className="progress">
-                        <div
-                          className="progress-bar"
-                          role="progressbar"
-                          style={{
-                            width: `${data.stats[2].base_stat}%`,
-                          }}
-                        >
-                          <small>{data.stats[2].base_stat}</small>
+                    <div className="tableRow">
+                      <div className="tableCell">DEF</div>
+                      <div className="tableCell">
+                        {" "}
+                        <div className="progress">
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            style={{
+                              width: `${data.stats[2].base_stat}%`,
+                            }}
+                          >
+                            <small>{data.stats[2].base_stat}</small>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="divTableRow">
-                    <div className="divTableCell">S.ATT</div>
-                    <div className="divTableCell">
-                      <div className="progress">
-                        <div
-                          className="progress-bar"
-                          role="progressbar"
-                          style={{
-                            width: `${data.stats[3].base_stat}%`,
-                          }}
-                        >
-                          <small>{data.stats[3].base_stat}</small>
+                    <div className="tableRow">
+                      <div className="tableCell">S.ATT</div>
+                      <div className="tableCell">
+                        <div className="progress">
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            style={{
+                              width: `${data.stats[3].base_stat}%`,
+                            }}
+                          >
+                            <small>{data.stats[3].base_stat}</small>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="divTableRow">
-                    <div className="divTableCell">S.DEF</div>
-                    <div className="divTableCell">
-                      <div className="progress">
-                        <div
-                          className="progress-bar"
-                          role="progressbar"
-                          style={{
-                            width: `${data.stats[4].base_stat}%`,
-                          }}
-                        >
-                          <small>{data.stats[4].base_stat}</small>
+                    <div className="tableRow">
+                      <div className="tableCell">S.DEF</div>
+                      <div className="tableCell">
+                        <div className="progress">
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            style={{
+                              width: `${data.stats[4].base_stat}%`,
+                            }}
+                          >
+                            <small>{data.stats[4].base_stat}</small>
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="divTableRow">
-                    <div className="divTableCell">SPD</div>
-                    <div className="divTableCell">
-                      <div className="progress">
-                        <div
-                          className="progress-bar"
-                          role="progressbar"
-                          style={{
-                            width: `${data.stats[5].base_stat}%`,
-                          }}
-                        >
-                          <small>{data.stats[5].base_stat}</small>
+                    <div className="tableRow">
+                      <div className="tableCell">SPD</div>
+                      <div className="tableCell">
+                        <div className="progress">
+                          <div
+                            className="progress-bar"
+                            role="progressbar"
+                            style={{
+                              width: `${data.stats[5].base_stat}%`,
+                            }}
+                          >
+                            <small>{data.stats[5].base_stat}</small>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
                 <div className="col-sm">
-                  <div className="divTableRow">
-                    <div className="divTableCell">HEIGHT:</div>
-                    <div className="divTableCell"> {data.height * 10}cm</div>
-                  </div>
-                  <div className="divTableRow">
-                    <div className="divTableCell">WEIGHT:</div>
-                    <div className="divTableCell"> {data.weight / 10}kg</div>
-                  </div>
-                  <div className="divTableRow">
-                    <div className="divTableCell">BASE EXP:</div>
-                    <div className="divTableCell"> {data.base_experience}</div>
-                  </div>
-                  <div className="divTableRow">
-                    <div className="divTableCell">ABILITIES:</div>
-                    <div className="divTableCell">
-                      {" "}
-                      {data.abilities.map((ability) => (
-                        <span>
-                          {ability.ability.name} <br></br>
-                        </span>
-                      ))}
+                  <div className="roundedBox">
+                    <div className="tableRow">
+                      <div className="tableCell">HEIGHT:</div>
+                      <div className="tableCell"> {data.height * 10}cm</div>
+                    </div>
+                    <div className="tableRow">
+                      <div className="tableCell">WEIGHT:</div>
+                      <div className="tableCell"> {data.weight / 10}kg</div>
+                    </div>
+                    <div className="tableRow">
+                      <div className="tableCell2"> BASE EXP:</div>
+                      <div className="tableCell"> {data.base_experience}</div>
+                    </div>
+                    <div className="tableRow">
+                      <div className="tableCell2">ABILITIES:</div>
+                      <div className="tableCell2">
+                        {" "}
+                        {
+                          // map for all abilities
+                          data.abilities.map((ability) => (
+                            <span>
+                              {ability.ability.name} <br></br>
+                            </span>
+                          ))
+                        }
+                      </div>
                     </div>
                   </div>
                 </div>
